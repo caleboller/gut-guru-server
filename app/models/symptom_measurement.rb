f7e1fcb4e0834ee2f1ebc6883098717e662cd379
Caleb Oller <caleboller@gmail.com>
2012-10-07 13:53:24 -0700

    make composite_score accessible

--- app/models/symptom_measurement.rb
+++ app/models/symptom_measurement.rb
@@ -1,6 +1,8 @@
 class SymptomMeasurement < ActiveRecord::Base
 	belongs_to :user
 
+	attr_accessible :composite_score
+
 	def suspect_food_entries_for_user user
 		FoodEntry.find(:all, :conditions => ["created_at >= ? AND user_id == ?", 4.hours.ago, user.id])
 	end
