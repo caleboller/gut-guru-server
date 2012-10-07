f23eb66634a42cfbbf6901f517151167d3393c47
Caleb Oller <caleboller@gmail.com>
2012-10-07 14:46:12 -0700

    fixes

--- app/controllers/api/v1/symptom_measurements_controller.rb
+++ app/controllers/api/v1/symptom_measurements_controller.rb
@@ -11,7 +11,18 @@ class Api::V1::SymptomMeasurementsController < ApplicationController
 	def report
 		top = current_user.top_symptom_measurements
 		bottom = current_user.bottom_symptom_measurements
+
 		
+		gluten = 0, lactose = 0, fat = 0
+		top.each do |measurement|
+			if measurement.suspect_food_entries_for_user(current_user)[0].gluten
+				gluten++ 
+			end
+			if measurement.suspect_food_entries_for_user(current_user)[0].lactose
+				lactose++
+			end
+		end
+		top_hash = {:lactose => lactose, :gluten => gluten, :}
 		top_hash = Hash.new()
 		top_hash["abdominal_pain"] = top.map{|x| x.abdominal_pain}.inject(0, :+)
 		top_hash["constipation"] = top.map{|x| x.constipation}.inject(0, :+)
