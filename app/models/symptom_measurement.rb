2dc6ff17e9c4aa690a89a88164fa8ceae1ead55e
Caleb Oller <caleboller@gmail.com>
2012-10-07 14:02:41 -0700

    fix for heroku

--- app/models/symptom_measurement.rb
+++ app/models/symptom_measurement.rb
@@ -4,7 +4,7 @@ class SymptomMeasurement < ActiveRecord::Base
 	attr_accessible :composite_score
 
 	def suspect_food_entries_for_user user
-		FoodEntry.find(:all, :conditions => ["created_at >= ? AND user_id == ?", 4.hours.ago, user.id])
+		FoodEntry.find(:all, :conditions => ["created_at >= ? AND user_id == ?", 4.hours.ago, user.id.to_s])
 	end
 
 	def composite_score
